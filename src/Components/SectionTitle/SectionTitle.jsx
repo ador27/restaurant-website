7c25b5b9335435047ac107c598c573ddf6745267
ador <rashedulhaque111@gmail.com>
2023-10-03 00:23:36 +0600

    Shared Section Completed

--- src/Components/SectionTitle/SectionTitle.jsx
+++ src/Components/SectionTitle/SectionTitle.jsx
@@ -1,8 +1,8 @@
 const SectionTitle = ({ heading, subHeading }) => {
     return (
-        <div>
-            <p className="text-yellow-500">---{subHeading}---</p>
-            <h3 className="text-4xl uppercase">{heading}</h3>
+        <div className="mx-auto text-center md:w-4/12 my-8">
+            <p className="text-yellow-500 mb-2">--- {subHeading} ---</p>
+            <h3 className="text-3xl uppercase border-y-4 py-4">{heading}</h3>
 
         </div>
     );
