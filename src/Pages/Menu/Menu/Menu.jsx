453f6b932f0b49ec923d3aa1053b0407ae802741
ador <rashedulhaque111@gmail.com>
2023-10-04 12:33:04 +0600

    Updated

--- src/Pages/Menu/Menu/Menu.jsx
+++ src/Pages/Menu/Menu/Menu.jsx
@@ -1,4 +1,5 @@
 import { Helmet } from 'react-helmet-async';
+import Cover from '../../Shared/Cover/Cover';
 
 const Menu = () => {
     return (
@@ -6,6 +7,7 @@ const Menu = () => {
             <Helmet>
                 <title>Delhi Restaurant | Menu</title>
             </Helmet>
+            <Cover></Cover>
             <h3>Men Menu Menuu</h3>
         </div>
     );
